788e3a474ac267e847785fa6e06ba01100919115
Matthew Gaston <matthew.gaston@broadcom.com>
2024-05-26 17:16:56 -0400

    formatting

--- src/views/AddRecipe.vue
+++ src/views/AddRecipe.vue
@@ -1,53 +1,52 @@
 <template>
-    <div>
-      <h2>Add Recipe</h2>
-      <form @submit.prevent="submitRecipe">
-        <div>
-          <label for="title">Title:</label>
-          <input v-model="title" id="title" required>
-        </div>
-        <div>
-          <label for="ingredients">Ingredients:</label>
-          <textarea v-model="ingredients" id="ingredients" required></textarea>
-        </div>
-        <div>
-          <label for="instructions">Instructions:</label>
-          <textarea v-model="instructions" id="instructions" required></textarea>
-        </div>
-        <button type="submit">Add Recipe</button>
-      </form>
-    </div>
-  </template>
-  
-  <script>
-  import { addRecipe } from '@/services/recipeService';
-  
-  export default {
-    name: 'AddRecipe',
-    data() {
-      return {
-        title: '',
-        ingredients: '',
-        instructions: ''
+  <div>
+    <h2>Add Recipe</h2>
+    <form @submit.prevent="submitRecipe">
+      <div>
+        <label for="title">Title:</label>
+        <input v-model="title" id="title" required>
+      </div>
+      <div>
+        <label for="ingredients">Ingredients:</label>
+        <textarea v-model="ingredients" id="ingredients" required></textarea>
+      </div>
+      <div>
+        <label for="instructions">Instructions:</label>
+        <textarea v-model="instructions" id="instructions" required></textarea>
+      </div>
+      <button type="submit">Add Recipe</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { addRecipe } from '@/services/recipeService';
+
+export default {
+  name: 'AddRecipe',
+  data() {
+    return {
+      title: '',
+      ingredients: '',
+      instructions: ''
+    };
+  },
+  methods: {
+    async submitRecipe() {
+      const recipe = {
+        title: this.title,
+        ingredients: this.ingredients.split('\n'),
+        instructions: this.instructions
       };
-    },
-    methods: {
-      async submitRecipe() {
-        const recipe = {
-          title: this.title,
-          ingredients: this.ingredients.split('\n'),
-          instructions: this.instructions
-        };
-        await addRecipe(recipe);
-        this.$router.push('/');
-      }
+      await addRecipe(recipe);
+      this.$router.push('/');
     }
-  };
-  </script>
-  
-  <style scoped>
-  h2 {
-    color: #42b983;
   }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+h2 {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,35 @@
 <template>
-    <div>
-      <h2>Home</h2>
-      <ul>
-        <li v-for="recipe in recipes" :key="recipe.id">{{ recipe.title }}</li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  import { getRecipes } from '@/services/recipeService';
-  
-  export default {
-    name: 'RecipeHomePage',
-    data() {
-      return {
-        recipes: []
-      };
-    },
-    async created() {
-      const response = await getRecipes();
-      this.recipes = response.data;
-    }
-  };
-  </script>
-  
-  <style scoped>
-  h2 {
-    color: #42b983;
+  <div>
+    <h2>Home</h2>
+    <!-- Add button -->
+    <router-link to="/add">
+      <button>Add Recipe</button>
+    </router-link>
+    <ul>
+      <li v-for="recipe in recipes" :key="recipe.id">{{ recipe.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { getRecipes } from '@/services/recipeService';
+
+export default {
+  name: 'RecipeHomePage',
+  data() {
+    return {
+      recipes: []
+    };
+  },
+  async created() {
+    const response = await getRecipes();
+    this.recipes = response.data;
   }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+h2 {
+  color: #42b983;
+}
+</style>
