2c2d4910f6861ba2bc0fc5d63547ade148ac1b54
Matthew Gaston <matthew.gaston@broadcom.com>
2024-05-26 16:25:44 -0400

    cleanup Add Recipe vue

--- src/App.vue
+++
@@ -1,3 +0,0 @@
-<template src="./views/AddRecipe/AddRecipe.html"></template>
-<script src="./views/AddRecipe/AddRecipe.js"></script>
-<style src="./views/AddRecipe/AddRecipe.css"></style>
---
+++ src/views/AddRecipe.vue
@@ -0,0 +1,3 @@
+<template src="./AddRecipe/AddRecipe.html"></template>
+<script src="./AddRecipe/AddRecipe.js"></script>
+<style src="./AddRecipe/AddRecipe.css"></style>
