d46a3c5853bf10387134b8326f1907b3fe1216f8
Matthew Gaston <matthew.gaston@broadcom.com>
2024-06-07 22:27:06 -0400

    trailing slash

--- src/services/recipeService.js
+++ src/services/recipeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/recipes';
+const API_URL = 'http://localhost:8000/api/recipes/';
 
 export const getRecipes = async () => {
   return await axios.get(API_URL);
@@ -21,9 +21,9 @@ export const searchRecipe = async (query) => {
   }
 };
 
-export const deleteRecipe = async (recipeId) => {
+export const deleteRecipe = async (id) => {
   try {
-    const response = await axios.delete(`${API_URL}/${recipeId}`);
+    const response = await axios.delete(`${API_URL}${id}`);
     return response.data;
   } catch (error) {
     console.error('Error deleting recipe:', error);
